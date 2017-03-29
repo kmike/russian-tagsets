0bb396105b30c590e2a8cd42301223d92af35b40
Nikolay Chervyakov <nick.chervyakov@gmail.com>
2017-03-29 14:47:38 +0700

    Fix encoding

--- setup.py
+++ setup.py
@@ -5,11 +5,14 @@ __version__ = '0.5.2'
 
 "Используем utf-8"
 
+r1 = open('README.rst', encoding='utf8').read()
+r2 = open('CHANGES.rst', encoding='utf8').read()
+
 setup(
     name="russian-tagsets",
     version=__version__,
     description="Russian tagset converters library",
-    long_description = open('README.rst').read() + "\n\n" + open('CHANGES.rst').read(),
+    long_description = r1 + "\n\n" + r2,
     license = 'MIT license',
     author='Mikhail Korobov',
     author_email='kmike84@gmail.com',
