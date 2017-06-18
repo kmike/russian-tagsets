e5addd10161f844872cf3220abcadf6e95e58b58
Mikhail Korobov <kmike84@gmail.com>
2017-06-18 05:14:24 +0500

    fixed discovery of dialog2017 tagset

--- russian_tagsets/__init__.py
+++ russian_tagsets/__init__.py
@@ -1,4 +1,12 @@
 from __future__ import absolute_import
 __version__ = '0.1'
 
-from . import aot, positional, dialog2010, opencorpora, ruscorpora, ud
+from . import (
+    aot,
+    positional,
+    dialog2010,
+    opencorpora,
+    ruscorpora,
+    ud,
+    dialog2017
+)
